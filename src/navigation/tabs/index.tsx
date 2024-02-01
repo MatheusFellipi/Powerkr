5ca29b072bcd9f2aec4b5d38e6d199299f172bbb
Matheus Fellipi <matheus.fellipi@outlook.com>
2024-02-01 17:21:03 -0300

    feat: add styles in tabs navigation

--- src/navigation/tabs/index.tsx
+++ src/navigation/tabs/index.tsx
@@ -3,12 +3,34 @@ import { Tabs } from "expo-router";
 
 export const NavigationTabs = () => {
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        tabBarStyle: {
+          borderTopLeftRadius: 24,
+          borderTopRightRadius: 24,
+          paddingBottom: 20,
+          paddingTop: 20,
+          backgroundColor: "#112439",
+          borderTopWidth: 0,
+          height: 97,
+          shadowColor: "#000",
+          shadowOffset: {
+            width: 0,
+            height: 7
+          },
+          shadowOpacity: 0.41,
+          shadowRadius: 9.11,
+          elevation: 14
+        }
+      }}
+    >
       {appStackTabs.map((item) => (
         <Tabs.Screen
           options={{
+            tabBarLabel: item.title,
             headerShown: false,
-            tabBarIcon: item.Icon
+            tabBarIcon: ({ focused }) =>
+              focused ? <item.activated /> : <item.Icon />
           }}
           name={item.name}
           key={item.name}
