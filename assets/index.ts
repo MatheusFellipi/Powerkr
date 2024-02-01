ba2925eb689231af232ed3adc085879b7f846225
Matheus Fellipi <matheus.fellipi@outlook.com>
2024-02-01 17:17:32 -0300

    feat: add svg

--- assets/index.ts
+++ assets/index.ts
@@ -1,13 +1,27 @@
+import Menu from "./svg/header/icon-menu.svg";
 import ArrowRight from "./svg/profile/icon-arrow-right.svg";
+import CircleHelp from "./svg/profile/icon-circle-help.svg";
+import Power from "./svg/profile/icon-power.svg";
 import Appointments from "./svg/tabs/icon-appointments.svg";
+import AppointmentsActivated from "./svg/tabs/icon-appointments-activated.svg";
 import Committees from "./svg/tabs/icon-committees.svg";
+import CommitteesActivated from "./svg/tabs/icon-committees-activated.svg";
 import Home from "./svg/tabs/icon-home.svg";
+import HomeActivated from "./svg/tabs/icon-home-activated.svg";
 import Profile from "./svg/tabs/icon-profile.svg";
+import ProfileActivated from "./svg/tabs/icon-profile-activated.svg";
 
 export const Icons = {
+  Menu,
   ArrowRight,
   Appointments,
   Committees,
   Home,
-  Profile
+  Profile,
+  Power,
+  AppointmentsActivated,
+  CommitteesActivated,
+  HomeActivated,
+  ProfileActivated,
+  CircleHelp
 };
