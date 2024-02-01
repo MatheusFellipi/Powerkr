51ecaae6f108afc003dcde6817270a8ae97cf851
Matheus Fellipi <matheus.fellipi@outlook.com>
2024-02-01 15:15:56 -0300

    feat: add index for assets icons

---
+++ assets/index.ts
@@ -0,0 +1,13 @@
+import ArrowRight from "./svg/profile/icon-arrow-right.svg";
+import Appointments from "./svg/tabs/icon-appointments.svg";
+import Committees from "./svg/tabs/icon-committees.svg";
+import Home from "./svg/tabs/icon-home.svg";
+import Profile from "./svg/tabs/icon-profile.svg";
+
+export const Icons = {
+  ArrowRight,
+  Appointments,
+  Committees,
+  Home,
+  Profile
+};
