05a6114e3ba8108481cfc48bd744c59b92f47565
Matheus Fellipi <matheus.fellipi@outlook.com>
2024-02-01 15:25:24 -0300

    feat: create theme for color in figma

--- src/app/(tabs)/profile.tsx
+++ src/app/(tabs)/profile.tsx
@@ -1,6 +1,50 @@
-import { MySafeAreaView } from "@shared/components/mySafeAreaView";
-import { Avatar, Text, View } from "tamagui";
+import { ProfileComponent } from "@components/profile";
+import { TextComponent } from "@shared/components/textComponent";
+import { Avatar, View } from "tamagui";
 
-export default function Home() {
-  return <MySafeAreaView bg="$blue10"></MySafeAreaView>;
+export default function Profile() {
+  return (
+    <View
+      pr="$4"
+      pl="$4"
+    >
+      <View ai="center">
+        <Avatar
+          circular
+          size="$15"
+        >
+          <Avatar.Image
+            accessibilityLabel="Cam"
+            src="https://images.unsplash.com/photo-1548142813-c348350df52b?&w=150&h=150&dpr=2&q=80"
+          />
+          <Avatar.Fallback backgroundColor="$blue10" />
+        </Avatar>
+        <TextComponent>Colaborador nome</TextComponent>
+        <TextComponent pin="description">Departamento</TextComponent>
+      </View>
+      <ProfileComponent.Options.OptionText
+        title="Nome"
+        description="Nome Completo"
+        handlePress={() => console.log("teste")}
+      />
+      <ProfileComponent.Options.OptionText
+        title="E-mail"
+        description="nome.sobrenome@gmail.com"
+        handlePress={() => console.log("teste")}
+      />
+      <ProfileComponent.Options.OptionText
+        description=""
+        title="Alterar senha"
+        handlePress={() => console.log("teste")}
+      />
+      <ProfileComponent.Options.OptionSwitch
+        title="Notificações"
+        handleSwitch={() => console.log("teste")}
+      />
+      <ProfileComponent.Options.OptionSwitch
+        title="Modo Noturno"
+        handleSwitch={() => console.log("teste")}
+      />
+    </View>
+  );
 }
---
+++ src/shared/components/viewComponent.ts
@@ -0,0 +1,7 @@
+import { styled, View } from "tamagui";
+
+export const ViewComponent = styled(View, {
+  variants: {
+    pin: {}
+  } as const
+});
